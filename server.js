660237c99e44f1e897203e94d28888dce6891f48
Michael Brookins <michael.brookins@rfsmart.com>
2024-11-18 09:45:37 -0500

    added files

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer( (req, res) => {
+//Get Current Path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// console.log(__filename, __dirname);
+
+const server = http.createServer(async(req, res) => {
     try {
 if(req.method === 'GET') {
+    let filePath;
     if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Hompage<h1>');
+        filePath = path.join(__dirname, 'public', 'index.html'); 
+        // res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.end('<h1>Hompage<h1>');
     } else if(req.url === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>About<h1>');
+        filePath = path.join(__dirname, 'public', 'about.html');  
+        // res.writeHead(200, {'Content-Type': 'text/html'});
+        // res.end('<h1>About</h1>');
     } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('<h1>Not Found<h1>');
+        throw new Error('Not Found');
+        // res.writeHead(404, {'Content-Type': 'text/html'});
+        // res.end('<h1>Not Found</h1>');
     }
+
+    const data = await fs.readFile(filePath);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(data);
+    res.end();
 } else {
     throw new Error('Method Not Allowed')
 }
     } catch (error) {
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        res.end('<h1>Server Error<h1>');
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Server Error');
     }
    
     // res.setHeader('Content-Type', 'text/html');
