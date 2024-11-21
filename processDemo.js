68a184fb56f373eeb342ad0a1d8f4500e42c9ed6
Michael Brookins <michael.brookins@rfsmart.com>
2024-11-21 17:20:59 -0500

    process demo

---
+++ processDemo.js
@@ -0,0 +1,33 @@
+// argv
+console.log(process.argv);
+console.log(process.argv[3]);
+
+//process.env
+console.log(process.env);
+console.log(process.env.COMPUTERNAME);
+console.log(process.env.USERNAME);
+
+//pid
+console.log(process.pid);
+
+//cwd
+console.log(process.cwd());
+
+//title
+console.log(process.title);
+
+//memoryUsage
+console.log(process.memoryUsage());
+
+//update()
+console.log(process.uptime());
+
+process.on('exit', (code) => {
+    console.log(`About to exit with code: ${code}`);
+})
+
+//exit()
+process.exit(0);
+    console.log('Hello from after exit');
+
+
