31378f693a90508670cc704c9b40d50920aee635
Michael Brookins <michael.brookins@rfsmart.com>
2024-11-21 14:54:04 -0500

    fs read, write, append

--- server2.js
+++ server2.js
@@ -61,6 +61,7 @@ const notFoundHandler = (req, res) => {
   res.end();
 };
 
+//Bring it all together
 const server = createServer((req, res) => {
   Logger(req, res, () => {
     jsonMiddleware(req, res, () => {
@@ -80,30 +81,6 @@ const server = createServer((req, res) => {
     });
   });
 });
-
-// if (req.url === '/api/users' && req.method === 'GET') {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.write(JSON.stringify(users));
-//     res.end();
-// } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
-//     const id = req.url.split('/') [3];
-//     const user = users.find((user) => user.id === parseInt(id));
-//     if (user) {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.write(JSON.stringify(user));
-//         res.end();
-//     } else {
-//         res.setHeader('Content-Type', 'application/json');
-//         res.statusCode = 404;
-//         res.write(JSON.stringify({ message: 'User not found' }));
-//         res.end();
-//     }
-// } else {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.statusCode = 404;
-//     res.write(JSON.stringify({ message: 'Route not found' }));
-//     res.end();
-// }
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
